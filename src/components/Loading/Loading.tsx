367ff631e0a1ad44b701ce632ee33586dda762ca
Vikas Patil <vikaspatil0021o+github@gmail.com>
2025-07-10 13:38:06 +0530

    created animated loading component & css

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,10 @@
+import "./Loading.css"
+
+export default function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner"></div>
+      <p className="loading-text">Loading...</p>
+    </div>
+  );
+}
